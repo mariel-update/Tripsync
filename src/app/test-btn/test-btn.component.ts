04cb0cc69e69efe262efbce2faad546a41a00869
Mariel <mariel0407@gmail.com>
2019-11-30 19:43:28 +0900

    テストコンポーネント作成

---
+++ src/app/test-btn/test-btn.component.ts
@@ -0,0 +1,36 @@
+import {Component, Inject} from '@angular/core';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+
+export interface DialogData {
+  animal: string;
+  name: string;
+}
+
+/**
+ * @title Dialog Overview
+ */
+@Component({
+  selector: 'test-btn.component',
+  templateUrl: 'test-btn.component.html',
+  styleUrls: ['test-btn.component.css'],
+})
+export class DialogOverviewExample {
+
+  animal: string;
+  name: string;
+
+  constructor(public dialog: MatDialog) {}
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
+      width: '250px',
+      data: {name: this.name, animal: this.animal}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      this.animal = result;
+    });
+  }
+
+}
